bc53a30f9db020444cbb8e44460d89519c05f21b
kasahun-welela <kasahunwelela1@gmail.com>
2025-02-11 23:40:45 +0300

    configure shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <h1>Test</h1>;
+  return (
+    <>
+      {" "}
+      <h1>Test</h1>
+      <Button>click me</Button>
+    </>
+  );
 }
