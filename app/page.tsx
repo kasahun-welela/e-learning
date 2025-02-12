d2d05e4bbf3e9cdb455dc95fd793b6f336ef2485
kasahun-welela <kasahunwelela1@gmail.com>
2025-02-12 18:20:59 +0300

    make section component reusable

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
 import Hero from "@/components/Hero";
 import PathCard from "@/components/PathCard";
+import SectionTitle from "@/components/SectionTitle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="max-w-[90%] mx-auto">
       <Hero />
-      <div className="py-6">
-        <h1 className="font-semibold text-3xl md:text-4xl">Benefits</h1>
-        <div className="space-y-4 md:flex justify-between">
-          <p className="text-sm md:text-base text-muted-foreground">
-            Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget
-            elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum
-            eget habitasse in velit fringilla feugiat senectus in.
-          </p>
-          <Button className="bg-white text-gray-900">View All</Button>
-        </div>
-      </div>
-
+      <SectionTitle
+        title="Benefits"
+        description="  Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget
+          elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget
+          habitasse in velit fringilla feugiat senectus in."
+      />
       <PathCard />
     </div>
   );
---
+++ components/SectionTitle.tsx
@@ -0,0 +1,20 @@
+import { title } from "process";
+import { Button } from "./ui/button";
+interface propsData {
+  title: string;
+  description: string;
+}
+
+export default function SectionTitle({ title, description }: propsData) {
+  return (
+    <div className="py-6">
+      <h1 className="font-semibold text-3xl md:text-4xl">{title}</h1>
+      <div className="space-y-4 md:flex justify-between items-center ">
+        <p className="text-sm md:text-base text-muted-foreground md:basis-3/5">
+          {description}
+        </p>
+        <Button className="bg-white text-gray-900">View All</Button>
+      </div>
+    </div>
+  );
+}
