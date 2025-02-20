97ce021ce0df59cc2b63247c83e335038f144e21
kasahun-welela <kasahunwelela1@gmail.com>
2025-02-20 15:12:24 +0300

    create about us page

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+export default function page() {
+  return (
+    <div className="max-w-[90%] mx-auto">
+      <div className="py-6 md:flex justify-between items-center">
+        <h1 className="font-semibold text-3xl md:text-4xl md:basis-1/2">
+          About Skillbridge
+        </h1>
+        <p className="text-sm md:text-base text-muted-foreground md:basis-1/2">
+          Welcome to our platform, where we are passionate about empowering
+          individuals to master the world of design and development. We offer a
+          wide range of online courses designed to equip learners with the
+          skills and knowledge needed to succeed in the ever-evolving digital
+          landscape.
+        </p>
+      </div>
+    </div>
+  );
+}
