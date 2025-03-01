a5d2f51e2a9bf802319539be1a4d43f08d5e953d
kasahun-welela <kasahunwelela1@gmail.com>
2025-03-01 21:59:23 +0300

    add footer component

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -3,6 +3,7 @@ import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -20,14 +21,23 @@ export default function page() {
         description="Welcome to our online course page, where you can enhance your skills in design and development. Choose from our carefully curated selection of 10 courses designed to provide you with comprehensive knowledge and practical experience. Explore the courses below and find the perfect fit for your learning journey."
       />
       <Card>
-        <CardHeader>
-          <CardTitle>Web Design Fundamentals</CardTitle>
-          <CardDescription>
-            Learn the fundamentals of web design, including HTML, CSS, and
-            responsive design principles. Develop the skills to create visually
-            appealing and user-friendly websites.
-          </CardDescription>
-          <Link href="/">View Course</Link>
+        <CardHeader className="md:flex justify-between">
+          <div>
+            <CardTitle>Web Design Fundamentals</CardTitle>
+            <CardDescription>
+              Learn the fundamentals of web design, including HTML, CSS, and
+              responsive design principles. Develop the skills to create
+              visually appealing and user-friendly websites.
+            </CardDescription>
+          </div>
+          <div>
+            <Link
+              href="/"
+              className="rounded p-3 border bg-gray-50 text-gray-800 hover:text-primary"
+            >
+              View Course
+            </Link>
+          </div>
         </CardHeader>
         <CardContent>
           <div className="md:px-6 flex justify-center gap-1 md:gap-2 lg:gap-5">
@@ -65,6 +75,37 @@ export default function page() {
             <p className="font-semibold">By Sara</p>
           </div>
         </CardContent>
+        <CardFooter>
+          <div className="px-4 rounded-xl border bg-card text-card-foreground">
+            <div className="border-b-2 w-full">
+              <h1 className="py-3 font-semibold">Curriculum</h1>
+            </div>
+            <div className="my-2 md:flex gap-4">
+              <div className="p-3 space-y-3 border-b md:border-b-0  md:border-l bg-card text-card-foreground">
+                <h1 className="text-4xl font-bold md:font-extrabold">01</h1>
+                <p className="font-semibold">Introduction to HTML</p>
+              </div>
+              <div className="p-3 space-y-3 border-b md:border-b-0  md:border-l bg-card text-card-foreground">
+                <h1 className="text-4xl font-bold md:font-extrabold">02</h1>
+                <p className="font-semibold">Styling with CSS</p>
+              </div>
+              <div className="p-3 space-y-3 border-b md:border-b-0  md:border-l bg-card text-card-foreground">
+                <h1 className="text-4xl font-bold md:font-extrabold">03</h1>
+                <p className="font-semibold">
+                  Introduction to Responsive Design
+                </p>
+              </div>
+              <div className="p-3 space-y-3 border-b md:border-b-0  md:border-l bg-card text-card-foreground">
+                <h1 className="text-4xl font-bold md:font-extrabold">04</h1>
+                <p className="font-semibold">Design Principles for Web</p>
+              </div>
+              <div className="p-3 space-y-3 border-b md:border-b-0  md:border-l bg-card text-card-foreground">
+                <h1 className="text-4xl font-bold md:font-extrabold">05</h1>
+                <p className="font-semibold">Building a Basic Website</p>
+              </div>
+            </div>
+          </div>
+        </CardFooter>
       </Card>
     </div>
   );
